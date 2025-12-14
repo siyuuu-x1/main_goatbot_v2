5ea9739a135304ae3b19e4d96d5c1a370a45d0f8
siyamislam1591-cyber <siyamislam1591@gmail.com>
2025-12-15 00:14:55 +0600

    Update autosetname.js

--- scripts/cmds/autosetname.js
+++ scripts/cmds/autosetname.js
@@ -6,7 +6,7 @@ function checkShortCut(nickname, uid, userName) {
 
 module.exports = {
 	config: {
-		name: "autosetname",
+		name: "siyuu",
 		version: "1.3",
 		author: "NTKhang",
 		cooldowns: 5,
