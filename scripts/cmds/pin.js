e5e4295ee00f5aba436ccbc58548035d98320cd4
siyamislam1591-cyber <siyamislam1591@gmail.com>
2025-12-15 09:53:27 +0600

    Update pin.js

--- scripts/cmds/pin.js
+++ scripts/cmds/pin.js
@@ -108,7 +108,7 @@ module.exports = {
     name: "pinterest",
     aliases: ["Pinterest", "pin"],
     version: "2.2",
-    author: "Mahi--",
+    author: "siyuuu",
     countDown: 10,
     role: 0,
     shortDescription: "Search Pinterest for images",
